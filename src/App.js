d68114f20c5cd15099cf64a5bc8fd417cd68eaa3
Alex Yu <j620656786206@gmail.com>
2018-01-13 20:45:55 -0600

    58. What is Firebase? & 59. Firebase Client Setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
 class App extends Component {
+	componentWillMount() {
+		firebase.initializeApp({
+			apiKey: 'AIzaSyBG-l5nkFpdz_whVxkeX8R2ylpp1UmEh7w',
+			authDomain: 'auth-ab0c3.firebaseapp.com',
+			databaseURL: 'https://auth-ab0c3.firebaseio.com',
+			projectId: 'auth-ab0c3',
+			storageBucket: 'auth-ab0c3.appspot.com',
+			messagingSenderId: '455716140808'
+		});
+	}
+
   render() {
 		return (
 			<View>
